e87fe688e9ab9ac89470300fa6099b5fefebcfa3
Réka <res@ronaldo.com>
2019-04-05 15:52:54 +0200

    the magic is real --- Please see comments in the file. 404 error removed, nothing shows though - but the scene doesn't seem to get the model (yet anyways). Please do remove the comments and the //REMOVABLE parts, those are just there to indicate the steps and that something is happening. console.log('FTW');

--- src/app/engine/engine.service.ts
+++ src/app/engine/engine.service.ts
@@ -2,6 +2,7 @@ import * as THREE from 'three-full';
 import { Injectable } from '@angular/core';
 import objLoader from 'three-obj-loader';
 import { GLTFLoader } from 'three';
+import { load } from '@angular/core/src/render3';
 
 
 
@@ -41,7 +42,7 @@ export class EngineService {
     // create the scene
     this.scene = new THREE.Scene();
 
-    
+
 
     this.camera = new THREE.PerspectiveCamera(
       75, window.innerWidth / window.innerHeight, 0.1, 1000
@@ -49,16 +50,38 @@ export class EngineService {
     this.camera.position.z = 5;
     this.scene.add(this.camera);
 
-    var loader = new THREE.GLTFLoader().setPath('./src/assets/glTF');
-    loader.load ('Avocado.gltf',function(gltf){
-gltf.scene.traverse(function(child){
-  if(child.isMesh){
-    var hej =1;
-  }
-})
-this.scene.add(gltf.scene);
+/*  
+ *  this was your original. I added a console log for the loader to see what it is, and such, and there you can see that it has a path variable using your method.
+ *  which, looking at the docs for three-loader, doesn't seem healthy.
+ *  see: https://www.npmjs.com/package/three-gltf-loader#usage
+ *  So I removed that part and again, as the Usage above shows, redid the loader.load (commented yours, just so that you have the comparison.)
+ *  For future reference, try console.log(#ANYTHING#), it usually has some clues that you can follow if in doubt :)
+ *  Good luck and let me know if you have any more webdev questions!
+ */
+//  var loader = new THREE.GLTFLoader().setPath('./src/assets/glTF');
+    var loader = new THREE.GLTFLoader();
+    console.log(loader); //Réka added this, REMOVABLE!
+    /*loader.load('Avocado.gltf', function (gltf) {
+      gltf.scene.traverse(function (child) {
+        if (child.isMesh) {
+          var hej = 1;
+        }
+      })
+      this.scene.add(gltf.scene);
+    });*/
+    loader.load(
+      '/assets/glTF/Avocado.gltf',
+      (gltf) => {
+        gltf.scene.traverse(function (child) {
+          if (child.isMesh) {
+            var hej = 1;
+            var hello = 'hello, Réka made it work...ish?';//Réka added this, REMOVABLE!
+            console.log(hello);//Réka added this, REMOVABLE!
+          }
+        })
+      this.scene.add(gltf.scene);
     });
-  
+
     
     // soft white light
     this.light = new THREE.AmbientLight(0x404040);
@@ -72,7 +95,7 @@ this.scene.add(gltf.scene);
     this.cube = new THREE.Mesh(geometry, material);
     //this.scene.add(this.cube);
   }
-  
+
 
   animate(): void {
     window.addEventListener('DOMContentLoaded', () => {
@@ -105,7 +128,7 @@ this.scene.add(gltf.scene);
   }
 }
 
-      
+
   //        this.gltfLoader.load(
   //       'C:\Users\Thomas\Documents\3DMiniProject\Cube.gltf',
   //       (gltf)=>{
@@ -115,4 +138,3 @@ this.scene.add(gltf.scene);
   //         console.log(`${(xhr.loaded / xhr.total *100)}% loaded`);
   //       }
   //     )
-      
